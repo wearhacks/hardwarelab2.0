7fabe09afd000fae1caaa0d387e5e7041776b02c
nadim <i.nadim@gmail.com>
2015-11-28 15:38:48 -0500

    Create a API for retrieving devices

--- hardwarelab/urls.py
+++ hardwarelab/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'hardwarelab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^devices/','rental.views.devices'),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 ]
--- rental/views.py
+++ rental/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
+from django.core import serializers
+from models import Device
 # Create your views here.
+def devices(request):
+  if request.method == 'GET':
+    json_devices = serializers.serialize("json", Device.objects.all())
+    return HttpResponse(json_devices, content_type='application/json')
