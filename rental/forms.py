65a201532be98c57c17be0a4d5a5f44574e291d5
Makrand <makrand1996@gmail.com>
2016-02-28 02:35:49 -0500

    merge cleanup

--- rental/forms.py
+++ rental/forms.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> c86d5a2279e6a27aa18a7daf622690f744d08262
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
@@ -11,8 +7,4 @@ from models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
-<<<<<<< HEAD
         fields = ('first_name', 'last_name', 'email')
-=======
-        fields = ('first_name', 'last_name', 'email')
->>>>>>> c86d5a2279e6a27aa18a7daf622690f744d08262
