0e5e83853102763efa38548bbe2dedbece57a5e3
Makrand <makrand1996@gmail.com>
2016-02-28 01:08:30 -0500

    Extended Users page on admin panel to include UserProfile model

--- rental/admin.py
+++ rental/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from rental.models import Device, Event, Inventory, Manufacturer, Review, Rental
+from rental.models import Device, Event, Inventory, Manufacturer, Review, Rental, UserProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.db.models import Q
 # Register your models here.
 
@@ -34,6 +36,11 @@ class ReviewAdmin(admin.ModelAdmin):
 class ReservationAdmin(admin.ModelAdmin):
   pass
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'users'
+
 class RentalAdmin(admin.ModelAdmin):
   list_display = ('user', 'returned', 'device', 'device_id')
   list_filter = ('returned','user', 'inventory__device')
@@ -42,7 +49,12 @@ class RentalAdmin(admin.ModelAdmin):
   def device_id(self,rental):
     return rental.inventory.id
 
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline, )
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Event,AdminEvent)
 admin.site.register(Device, DeviceInline)
 admin.site.register(Inventory, InventoryAdmin)
