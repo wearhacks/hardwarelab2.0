16c03a5bc4c2fbfb008993817c0a367ffac97f54
nadim <i.nadim@gmail.com>
2016-10-07 00:25:39 -0400

    database

--- hardwarelab/settings.py
+++ hardwarelab/settings.py
@@ -92,12 +92,12 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
